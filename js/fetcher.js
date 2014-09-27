4ccac2cad545cb80c5fe0c2b16bc6e7e2e9a77dc
sturdevant <sturdevant2@wisc.edu>
2014-09-27 01:58:15 -0500

    Plots the crab!
    Need to modify on click events to fetch new data & plot it

---
+++ js/fetcher.js
@@ -0,0 +1,118 @@
+var xhr = new XMLHttpRequest();
+var db = undefined;
+var loaded = function(){return;};
+//xhr.open('GET', 'hawc_test_lightcurves_repaired.db', true);
+xhr.open('GET', 'hawc_test_lightcurves_copy.db', true);
+xhr.responseType = 'arraybuffer';
+
+// Send request for sql file
+xhr.send();
+// What to do when db is loaded
+function set_loaded (func) {loaded = func;}
+
+// Find gaps within the data
+function find_gap(arr) {
+   var m_diff = .0014; // 2 minute gap in mjd
+   var gaps = [];
+   for (var i = 1; i < arr.length; i++) {
+      if (arr[i] - arr[i-1] > m_diff) {
+         gaps.push(i-1);
+         gaps.push(i);
+      }
+   }
+   
+   return gaps;
+}   
+
+// Given a database file & name of source, returns MJD, on & off data
+function fetch(name, start, end) {
+   console.log("was called");
+   // Make sure db is already loaded
+   if (typeof db === 'undefined') {
+      return;
+   }
+   
+   // Load arrays within date range
+   var range = " WHERE MJD BETWEEN " + start + " AND " + end;
+   var mjd_tbl = db.exec("SELECT MJD FROM lightcurve_on" + range)[0];
+   var on_tbl = db.exec("SELECT "+ name + " FROM lightcurve_on" + range)[0];
+   var off_tbl = db.exec("SELECT " + name + " FROM lightcurve_off" + range)[0];
+   
+   // Detect gaps in data
+   var gaps = find_gap(mjd_tbl.values);
+   
+   // Make sure we have arrays of equal length (chop off last elt's)
+   var lMjd = mjd_tbl.values.length;
+   var lOn = on_tbl.values.length;
+   var lOff = off_tbl.values.length;
+   var l = d3.min([lMjd, lOn, lOff]);
+   
+   // Average on # of days, O(1) data points in # of days instead of O(n)
+   // Shouldn't need high resolution when looking at a long timeframe anyway!
+   var days = Math.ceil((mjd_tbl.values[l-1] - mjd_tbl.values[0])/2);
+   
+   // Throw out remainder points
+   l = l - l % days;
+   // data will contain json points w/ on, off, diff, rat & mjd values
+   var dta = [];
+   // number of consecutive zeros encountered
+   var z = 0;
+   // array to keep track of prev/current/next value was a gap
+   var g = [false, false, false];
+   var cd = 0;
+   
+   for (var i = 0; i < l; i += days) {
+      // Average mjd of range
+      var mjd = .5*(+mjd_tbl.values[i+days-1] + +mjd_tbl.values[i]);
+      var on = 0;
+      var off = 0;
+      var gn = false;
+      var gap = -1000000;// Set to -1 & see idea below
+      
+      // Average values to reduce # of data points, 
+      // slightly reduces resolution, but vastly improves performance!
+      for (var j = i; j < days + i; j++) {
+         on += +on_tbl.values[j];
+         off += +off_tbl.values[j];
+         // Check if next point will have a gap
+         if (gaps.indexOf(j+days) != -1)
+            gn = true;
+      }
+      
+      // If prev, curr or next are gaps, don't display this point
+      g.shift();
+      g.push(gn);
+      if (g[0] || g[1] || g[2])
+         gap = 1000000;// Set to 1 & see idea below
+      
+      // Either increment if this is zero or reset zero counter
+      on == 0 && off == 0 ? z++ : z = 0;
+      // Every eleven zeros, remove the middle (so that we have a padding of 5
+      // zero points, makes the interpolation look nicer than no padding!)
+      if (z == 11) {
+         z = 10;
+         dta.splice(dta.length - 6, 1);
+      }
+      
+      var exc = on - off;
+      ce += diff;
+      var rat = off == 0 ? 0 : on/off;
+      dta.push({mjd: mjd,
+         on: on/days,
+         off: off/days,
+         exc: diff/days,
+         ce: ce,
+         rat: rat,
+         gap: gap});// do gap*(max(on, off, diff, rat, cd)+1)?
+   }
+   
+   return dta;
+}
+
+// When db is loaded, store into a var
+xhr.onload = function(e) {
+   // Load database into db
+   var uInt8Array = new Uint8Array(this.response);
+   db = new SQL.Database(uInt8Array);
+   loaded();
+};
---
+++ js/plotter.js
@@ -0,0 +1,205 @@
+function add_plot(eltid) {
+   // Set up HTML elements
+   var g = document.getElementById(eltid);
+   var svg = d3.select("#" + eltid).append("svg");
+   var focus = svg.append("g").attr("class","focus");
+   var context = svg.append("g").attr("class","context");
+   
+   // Declare variables for plot dimensions & axes
+   var height, height2, width;
+   var x = d3.scale.linear();
+   var y = d3.scale.linear();
+   var x2 = d3.scale.linear();
+   var y2 = d3.scale.linear();
+   var xAxis = d3.svg.axis().orient("bottom");
+   var xAxis2 = d3.svg.axis().orient("bottom");
+   var yAxis = d3.svg.axis().orient("left");
+
+   // Arrays of interpolations for focus, context & gap, along with selectors
+   var foc = [];
+   var con = [];
+   var gap = [];
+   
+   // Set brush
+   var brushed = function() {
+      x.domain(brush.empty() ? x2.domain() : brush.extent());
+      for (var i = 0; i < foc.length; i++)
+         foc[i].p.attr("d", foc[i].i);
+      focus.select(".x.axis").call(xAxis);
+   }
+   
+   var brush = d3.svg.brush()
+      .on("brush", brushed);
+
+   var set_axes = function() {
+      // Clear prev graph
+      focus.selectAll("g").remove();
+      context.selectAll("g").remove();
+      
+      // Set ranges
+      x.range([0, width]);
+      x2.range([0, width]);
+      y.range([height, 0]);
+      y2.range([height2, 0]);
+      
+      // Scale axes
+      xAxis.scale(x);
+      xAxis2.scale(x2);
+      yAxis.scale(y);
+
+      // set x & y for focus
+      focus.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + height + ")")
+         .call(xAxis);
+      
+      focus.append("g")
+         .attr("class", "y axis")
+         .call(yAxis);
+      
+      // x & brush for context
+      context.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + height2 + ")")
+         .call(xAxis2);
+      
+      context.append("g")
+         .attr("class", "x brush")
+         .call(brush)
+         .selectAll("rect")
+         .attr("y", -6) // Okay for now, but might want to make relative later
+         .attr("height", height2 + 7); 
+       
+      brush.x(x2);
+   } 
+   
+   // Update widths and heights on window resize
+   var updateWindow = function() {
+      // Get total available space for x & y
+      var ex = g.clientWidth;
+      var ey = g.clientHeight;
+      
+      // Set margins
+      var margin = {top: .02*ey, right: .01*ex, bottom: .2*ey, left: .04*ex},
+         margin2 = {top: .86*ey, right: .01*ex, bottom: .04*ey, left: .04*ex};
+      width = ex - margin.left - margin.right;
+      height = ey - margin.top - margin.bottom;
+      height2 = ey - margin2.top - margin2.bottom;
+       
+      // Clip paths so they don't go outside of width & height
+      svg.append("defs").append("clipPath")
+         .attr("id", "clip")
+         .append("rect")
+         .attr("width", width)
+         .attr("height", height);
+      
+      // Set width & height of svg, retranslate focus & context
+      svg.attr("width", width + margin.left + margin.right)
+         .attr("height", height + margin.top + margin.bottom);
+      focus.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      context.attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+      
+      set_axes();
+      // Redraw graphs to new dimensions
+      for (var i = 0; i < foc.length; i++){
+         foc[i].i.y0(height);
+         foc[i].p.attr("d", foc[i].i);
+      }
+      for (var i = 0; i < con.length; i++){
+         con[i].i.y0(height2);
+         con[i].p.attr("d", con[i].i);
+      }
+   }
+   // Call on resize & make sure dimensions are initialized before plotting!
+   window.onresize = updateWindow;
+   updateWindow();
+   
+   // Moves selection to front
+   d3.selection.prototype.moveToFront = function() {
+      return this.each(function() {
+         this.parentNode.appendChild(this);
+      });
+   };
+   
+   // Returns an interpolation given type & x & y functions
+   var get_intpl = function (type, fx, fy, hght) {
+      return d3.svg.area().interpolate(type).x(fx).y0(hght).y1(fy); 
+   }
+   
+   // Determines min & max values of functions given data, sets axis domain
+   var set_domain = function (ax, fd) {
+      var min = Number.MAX_VALUE;
+      var max = Number.MIN_VALUE;
+
+      for (var i = 0; i < fd.length; i++) {
+          min = d3.min([min, d3.min(fd[i].data.map(fd[i].f))]);
+          max = d3.max([max, d3.max(fd[i].data.map(fd[i].f))]);
+      }
+      ax.domain([min, max]);
+   }
+   
+   var plot = function(p) {
+      // Scale functions w/ x & y
+      var fx = function(d) { return p.x(p.fx(d));};
+      var fy = function(d) { return p.y(p.fy(d));};
+      
+      // Add interpolation to graph
+      var intpl = get_intpl(p.type, fx, fy, p.height);
+      var path = p.c.append("path")
+         .datum(p.data)
+         .attr("class",p.class)
+         .attr("d", intpl)
+         .on("mouseover", p.hover);
+
+      return {i:intpl, p:path};
+   }
+
+   // Hover function
+   var hover = function() {
+      // Cool effect to bring hovered el't to front
+      d3.select(this).moveToFront();
+      d3.selectAll(".gap").moveToFront();
+      d3.selectAll(".axis").moveToFront();
+   }
+   var none = function() {}
+
+   function set_plots(f, c, g) {
+      // Clear previous plot (if there was one)
+      focus.selectAll("path").remove();
+      context.selectAll("path").remove();
+
+      // Set domains of axes in focus
+      var fdx = [];
+      var fdy = [];
+      for (var i = 0; i < f.length; i++) {
+         fdx.push({data:f[i].data, f:f[i].fx});
+         fdy.push({data:f[i].data, f:f[i].fy});
+      }
+      set_domain(x, fdx);
+      set_domain(y, fdy);
+      
+      // Set domains of axes in context
+      fdx = [];
+      fdy = [];
+      for (var i = 0; i < c.length; i++) {
+         fdx.push({data:c[i].data, f:c[i].fx});
+         fdy.push({data:c[i].data, f:c[i].fy});
+      }
+      set_domain(x2, fdx);
+      set_domain(y2, fdy);
+      set_axes();
+      
+      // Go through lists again (seems inefficient, better way??)
+      for (var i = 0; i < f.length; i++) {
+         foc.push(plot( {c:focus, x:x, y:y, height:height, hover:hover,
+            fx:f[i].fx, fy:f[i].fy, type:"basis", class:f[i].class, 
+            data:f[i].data}));
+      }
+      for (var i = 0; i < c.length; i++) {
+         con.push(plot( {c:context, x:x2, y:y2, height:height2, hover:none,
+            fx:c[i].fx, fy:c[i].fy, type:"basis", class:c[i].class, 
+            data:c[i].data}));
+      }
+   }
+   return set_plots;
+}
